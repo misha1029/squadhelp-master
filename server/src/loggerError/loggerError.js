83dccbc252f084433bfe38baa81bf0bcc58c1e4f
misha1029 <sherstoun001@gmail.com>
2020-05-07 10:48:08 +0300

    Add: ErrorLogFile.json

--- server/src/loggerError/loggerError.js
+++ server/src/loggerError/loggerError.js
@@ -3,13 +3,13 @@ import moment from 'moment';
 import CONSTANTS from './../constants';
 
 module.exports =  (err) => {
-	const data = {
+	const option = {
 		MESSAGE: err.message,
 		TIME: moment().format('LLLL'),
 		CODE: err.code,
-		STACKTRACE: err,
+		STACKTRACE: err.stack,
 	};
-	fs.writeFile(CONSTANTS.ERROR_LOGGER_FILE, JSON.stringify(data)+ ',' + '\n', { flag:'a' }, (err) => {
+	fs.writeFile(CONSTANTS.ERROR_LOGGER_FILE, JSON.stringify(option)+ ',' + '\n', { flag:'a' }, (err) => {
 		if (err)
 			throw err;
 	});
